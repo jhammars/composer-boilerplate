40ec2fd210225605163b68c35fc4c3555f8dcf57
dolebas <johan.hammarstrm@gmail.com>
2017-08-03 20:40:50 +0200

    Update dolebas-c9-conf.sh

--- dolebas-c9-conf.sh
+++ dolebas-c9-conf.sh
@@ -3,9 +3,12 @@
 
 echo "Adding vendor/bin to PATH"
 export PATH="/home/ubuntu/workspace/vendor/bin:$PATH"
+
 echo "Setting /web as web root"
-sudo mv ~/workspace/001-cloud9.conf 
+sudo mv ~/workspace/001-cloud9.conf /etc/apache2/sites-available/001-cloud9.conf
+
 echo "Adding settings.local.php to /web/sites/default"
 sudo mv ~/workspace/settings.local.php ~/workspace/web/sites/default/settings.local.php
+
 echo "Adding .htaccess to /web"
 cp ~/workspace/.htaccess ~/workspace/web/.htaccess
